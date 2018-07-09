245e9dfad976148b4e89e6f82c6cb5881ddb40c0
gikacred <gikacred@gmail.com>
2018-07-09 12:06:27 -0300

    funcao de envio

--- app.js
+++ app.js
@@ -66,9 +66,8 @@ function processPostback(event) {
       }
 
       var message = greeting + "Eu sou seu atendente do Batata. Neste aplicativo você pode oferecer e solicitar diversos tipos de serviços. Escolha uma das opções abaixo:";
-      //sendMessage(senderId, {text: message});
+      sendMessage(senderId, {text: message});
 
-      respostaInicial(senderId, message);
 
     });
 
@@ -91,10 +90,18 @@ function processPostback(event) {
       }
 
       var message = "Legal " + name + ", assista o tutorial abaixo para saber como oferecer o ser serviço pelo Batata e baixe o aplicativo através do link: ";
+      
       sendMessage(senderId, {text: message});
 
-
-
+      sendMessage(senderId, {buttons: [{
+                      		type: "postback",
+                      		title: "Cliente",
+                      		payload: "Cliente"
+                    		}, {
+                      		type: "postback",
+                      		title: "Profissional",
+                      		payload: "Profissional"
+                    	}]});
     });
 
 
@@ -140,41 +147,6 @@ function sendMessage(recipientId, message) {
   });
 }
 
-function respostaInicial(userId, message){
-
-	message = {
-
-			attachment = {
-				
-				type: "template",
-
-				payload: {
-
-					template: "generic",
-
-					elements:[{
-
-						title: message,
-
-						buttons: [{
-                      		type: "postback",
-                      		title: "Cliente",
-                      		payload: "Cliente"
-                    		}, {
-                      		type: "postback",
-                      		title: "Profissional",
-                      		payload: "Profissional"
-                    	}]
-
-					}]
-				}
-			}
-
-	};
-
-	sendMessage(userId, message);
-
-}
 
 /*
 function findMovie(userId, movieTitle) {
