03b21d402436b3e2d6dee5fdbe5a231ffa3d98d2
gikacred <gikacred@gmail.com>
2018-07-10 10:00:55 -0300

    generic Messager

--- app.js
+++ app.js
@@ -119,24 +119,14 @@ function sendMessageWithButton(sender){
 				"elements": [{
 					"title": "First card",
 					"subtitle": "Element #1 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
 					"buttons": [{
-						"type": "web_url",
-						"url": "https://www.messenger.com",
-						"title": "web url"
+						"type": "postback",
+						"title": "Sou  profissional",
+						"payload": "profissional",
 					}, {
 						"type": "postback",
-						"title": "Postback",
-						"payload": "Payload for first element in a generic bubble",
-					}],
-				}, {
-					"title": "Second card",
-					"subtitle": "Element #2 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-					"buttons": [{
-						"type": "postback",
-						"title": "Postback",
-						"payload": "Payload for second element in a generic bubble",
+						"title": "Procuro serviços",
+						"payload": "cliente",
 					}],
 				}]
 			}
