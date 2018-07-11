7e7568b4a9ce881acf68caa1a05f2a193b695337
gikacred <gikacred@gmail.com>
2018-07-11 11:45:38 -0300

    firebase novo

--- services/firebase.js
+++ services/firebase.js
@@ -1,11 +1,11 @@
 //var firebase 		= require('firebase');
 var admin 			= require('firebase-admin');
-var serviceAccount 	= require('../config/service-account');
+var serviceAccount 	= require('../config/service-account.json');
 
 var firebase = admin.initializeApp({
 
 	credential: admin.credential.cert(serviceAccount),
-	databaseURL: 'https://trampoapp-9510a.firebaseio.com/'
+	databaseURL: 'https://trampoapp-9510a.firebaseio.com/',
 
 });
 
